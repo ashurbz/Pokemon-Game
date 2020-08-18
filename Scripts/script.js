e31257f8902191bb0c5b0a284824d722418e40c0
ashurbz <rbzashu1@gmail.com>
2020-08-18 12:54:51 +0530

    favicon added

--- Scripts/script.js
+++ Scripts/script.js
@@ -211,6 +211,8 @@ function updateGameArea() {
                count++;
             balls[j].update();
             if( (balls[j].x-myGamePiece.x >-30  && balls[j].x-myGamePiece.x <= 30 &&  balls[j].y-myGamePiece.y <= 30 &&  balls[j].y-myGamePiece.y >-30) ){
+                var sound = new Audio("./Sounds/sound1.mp3");
+                sound.play();
                 
                 balls[j] = "A";
                 counter++;
